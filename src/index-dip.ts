8ebd669446c4c2bed172ae0b6a9fe5f22401b77f
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-05-14 06:53:57 +0300

    refactor: misc

--- src/index-dip.ts
+++
@@ -1,8 +0,0 @@
-import { ConsoleLogger } from "./ConsoleLogger";
-import { CounterInteractor } from "./CounterInteractor";
-
-const counterInteractor = new CounterInteractor(new ConsoleLogger())
-
-counterInteractor.increment()
-counterInteractor.increment()
-counterInteractor.increment()
--- src/index-without-dip.ts
+++
@@ -1,6 +0,0 @@
-import {PostInteractor} from "./business-logic-without-dip";
-
-new PostInteractor().getPostTitle('https://my-json-server.typicode.com/typicode/demo/posts')
-  .then(title => {
-    console.log(`title = ${title}` )
-  })
